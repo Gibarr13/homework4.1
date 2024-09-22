ce965edc96ddbc3ac9290c77ac77e0729b909bea
George Ibarra <gibarr13@stu.southtexascollege.edu>
2024-09-22 12:14:22 -0500

    completed Expense Tracker

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
   import IncomeExpenses from './components/IncomeExpenses.vue';
   import AddTransactions from './components/AddTransactions.vue';
   import TransactionList from './components/TransactionList.vue';
-  import {ref, computed} from 'vue'
+  import {ref, computed, onMounted} from 'vue'
 
   const transactions = ref([])
 
@@ -35,12 +35,26 @@
       text: transactionData.text,
       amount: transactionData.amount,
     })
+    saveToLocalStorage()
   }
 
   const handleDelete = (id) => {
     transactions.value = transactions.value.filter((x) => x.id !== id)
+    saveToLocalStorage()
   }
 
+  const saveToLocalStorage = () => {
+    localStorage.setItem('transactions', JSON.stringify(transactions.value))
+  }
+
+  onMounted(() => {
+    const savedtransactions = JSON.parse(localStorage.getItem('transactions'))
+  
+
+  if(savedTransaction){
+    transactions.value = savedTransactions
+  }
+})
 </script>
 
 <template>
